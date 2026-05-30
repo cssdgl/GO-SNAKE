17342801fb04648a4a44ec2210ce00e26927d205
cssdgl <cassandradogliani@icloud.com>
2026-05-30 22:32:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,4 +5,10 @@
     "react-native-google-mobile-ads": "latest",
     "react-native-tracking-transparency": "latest"
   }
-}
+{
+  "expo": {
+    "name": "GO SNAKE",
+    "slug": "go-snake",
+    "ios": {
+      "bundleIdentifier": "com.tonnom.gosnake"
+    }
